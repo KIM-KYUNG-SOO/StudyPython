e33422bcfb95e980ab227fbc79b860421a52f297
KIM-KYUNG-SOO <kks6719@gmail.com>
2022-01-14 17:09:33 +0900

    파이썬 1일차 진행

---
+++ day1/basic_syntax01.py
@@ -0,0 +1,33 @@
+# 주석입니다
+# 문저열 표현방법
+print('Hello, World')
+print("Hello, World") #둘다 사용가능하다. '를 많이씀
+
+# 숫자연산 출력, print()활용
+print(3+4)
+
+# 변수사용
+a = '안녕하세요'
+b = '반갑습니다!!'
+print(a)
+print(b)
+print(a, b)
+print(a, b, '정말?')
+
+#수학연산
+print(5 + 6)
+print(5 - 6)
+print(5 * 6)
+print(5 / 3)
+print(5 // 3) # 정수로만 나옴
+print(5 % 3) # 나머지
+print(2**10) # 거듭제곱
+
+print(int(5/3))
+print(int(10))
+#print(int('ten')) # 형변환 안됨
+print(float(5)) # 소수점 출력
+
+#Type 확인
+print(type(10))
+print(type(10.0))
